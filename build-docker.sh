bdac62631ac046015de1202bff7b927c02cb0031
Michael Lahr <michael.lahr@gmail.com>
2025-06-02 10:42:41 +0800

    build: add multi-platform Docker build support for sonar scan

--- build-docker.sh
+++ build-docker.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 poetry install
-poetry run python -m tfc_code_pipeline.cli  --build-only --cmd sonar_scan
+poetry run python -m tfc_code_pipeline.cli  --build-only --cmd sonar_scan --platform linux/arm64
+poetry run python -m tfc_code_pipeline.cli  --build-only --cmd sonar_scan --platform linux/amd64
